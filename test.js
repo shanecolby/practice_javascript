6be3176b92569cc32d7e07c9a870f3fa4c859e9b
shane <shanecolbyzero8@gmail.com>
2020-12-06 10:34:00 -0600

    java practice

--- test.js
+++ test.js
@@ -59,81 +59,100 @@
 
 // Write a method that prints out every number from 1 to 100. 
 
-function printNumbers() {
-  var i = 0;
-  while (i < 100) {
-    console.log(i);
-    i++;
-  }
+// function printNumbers() {
+//   var i = 0;
+//   while (i < 100) {
+//     console.log(i);
+//     i++;
+//   }
 
-}
-printNumbers();
+// }
+// printNumbers();
+
+// function printNumbers() {
+//   var i = 0;
+//   while (i <= 100) {
+//     console.log(i);
+//     i++
+//   }
+// }
+// printNumbers();
 
 
 // // Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
 
-function everyOtherNumber() {
-  var i = 1;
-  while (i < 100) {
-    console.log(i);
-    i += 2;
-  }
-}
-everyOtherNumber();
+// function everyOtherNumber() {
+//   var i = 1;
+//   while (i <= 100) {
+//     console.log(i);
+//     i += 2
+//   }
+// }
+// everyOtherNumber();
+
+// function everyOtherNumber() {
+//   var i = 1;
+//   while (i < 100) {
+//     console.log(i);
+//     i += 2;
+//   }
+// }
+// everyOtherNumber();
 
 
 // Write a method that accepts an array of numbers as a parameter, and counts how many 55’s there are in the array.
 
-function repeatNumber(array) {
-  var count = 0;
-  array.forEach(function (number) {
-    if (number === 55) {
-      count += 1;
-    }
-    console.log(number);
+// function repeatNumber(array) {
+//   var count = 0;
+//   array.forEach(function (number) {
+//     if (number === 55) {
+//       count += 1;
+//     }
+//     console.log(number);
 
-  });
-  return count
+//   });
+//   return count
 
-}
-console.log(repeatNumber(number));
+// }
+// console.log(repeatNumber(number));
 
 
 // Write a method that accepts an array of strings and returns a new array that has the string "awesomesauce" inserted between every string. 
 // For example, if the initial array is ["a", "b", "c", "d", "e"], then the returned array should be ["a", "awesomesauce", "b", "awesomesauce", "c", "awesomesauce", "d", "awesomesauce", "e"].
 
-function awesomesauced(strings) {
-  var awesomeArray = [];
-  strings.forEach(function (string) {
-    awesomeArray.push(string);
-    awesomeArray.push("awesomesauce");
+// function awesomesauced(strings) {
+//   var awesomeArray = [];
+//   strings.forEach(function (string) {
+//     awesomeArray.push(string);
+//     awesomeArray.push("awesomesauce");
 
-  });
-  awesomeArray.pop();
-  return awesomeArray;
-}
+//   });
+//   awesomeArray.pop();
+//   return awesomeArray;
+// }
 
-console.log(awesomesauced(["a", "b", "c", "d"]))
+// console.log(awesomesauced(["a", "b", "c", "d"]))
 
 // Start with the hash: item_amounts = {chair: 5, table: 2}
 // Someone just bought two chairs. Change the hash such that the chair amount is 3.
 // The final result should be: {chair: 3, table: 2}
-var itemAmounts = (chair: 5, table: 2);
-itemAmounts["chair"] = 3;
 
-console.log(itemAmounts);
+// var itemAmounts = (chair: 5, table: 2);
+// itemAmounts["chair"] = 3;
+
+// console.log(itemAmounts);
 
 
 // Start with the hash: item_amounts = {chair: 5, table: 2}
 // You received 7 desks to sell. Change the hash to include desks.
 // The final result should be: {chair: 5, table: 2, desk: 7}
 
-var itemAmounts = (chair: 5, table: 2);
+// var itemAmounts = (chair: 5, table: 2);
 
-itemAmounts.desk = 7;
-// itemAmounts["desk"] = 7;
+// itemAmounts.desk = 7;
+// // itemAmounts["desk"] = 7;
 
-console.log(itemAmounts);
+// console.log(itemAmounts);
 
 
 // Write a method that accepts a number and returns its factorial.
@@ -142,3 +161,72 @@ console.log(itemAmounts);
 
 // Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array. 
 // For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+
+// Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
+
+// function selectEvenItems(strings) {
+//   var result = [];
+//   var index = 0;
+//   strings.forEach(function (string) {
+//     if (index % 2 === 0) {
+//       result.push(string)
+//     }
+//   });
+//   return result
+// }
+// console.log(selectEvenItems(["a", "b", "c", "d", "e", "f"]))
+
+
+// Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+
+// function max(numbers) {
+//   var current_max = numbers[0];
+//   numbers.forEach(function (number) {
+//     if (number > current_max) {
+//       (current_max = number)
+//     };
+//   });
+//   return current_max
+// }
+// console.log(max([5, 4, 8, 1, 2]))
+
+
+// Write a method that accepts a number and returns its factorial. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+function factorial(number) {
+  var result = 1;
+  var current_number = number;
+  for (var i = 0; i < number; i++) {
+    result = (result * current_number);
+    current_number = (current_number - 1);
+  }
+  return result
+}
+console.log(factorial(5));
+
+
+// Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built into Ruby.
+
+function descending(numbers) {
+  var result = [];
+  var index = numbers.length - 1;
+  for (var i = 0; i < numbers.length; i++) {
+    result.push(numbers[index]);
+    index = index - 1
+
+  }
+  return result;
+}
+console.log(descending([1, 3, 5, 7]));
+
+
+// Write a method that accepts two arrays of numbers, and returns an array of every sum of every combination of single numbers from the first and second array. For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should return this array: [101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+def sum_combinations(numbers1, numbers2)
+result = []
+numbers1.each do | number1 |
+  numbers2.each do | number2 |
+    result << number1 + number2
+    end
+  end
+result
+end
+p sum_combinations([1, 5, 10], [100, 500, 1000])
--- testloop.rb
+++ testloop.rb
@@ -47,57 +47,73 @@
 
 # # Write a method that prints out every number from 1 to 100. 
 
-def print_numbers
-  index = 0
-  while index < 100
-    p index
-    index += 1
-  end
+# def print_numbers
+#   index = 0
+#   while index < 100
+#     p index
+#     index += 1
+#   end
 
 
 
-end
-print_numbers
+# end
+# print_numbers
 
-
+# def print_numbers
+#   i = 0
+#   while i <= 100
+#     p i
+#     i += 1
+#   end
+# end
+# print_numbers
 # Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
 
-def every_other_number()
-  index = 1
-  while index <= 100
-    p index 
-    index += 2
-  end
-end
-every_other_number()
+# def every_other_number()
+#   index = 1
+#   while index <= 100
+#     p index 
+#     index += 2
+#   end
+# end
+# every_other_number()
 
+# def every_other_number
+#   i = 1
+#   while i <= 100
+#     p i
+#     i += 2
+#   end
+# end
+# every_other_number
 
 # Write a method that accepts an array of numbers as a parameter, and counts how many 55’s there are in the array.
 
 
-def repeat_number(array)
-  array = [2,55,4,7,55]
-  repeat_number = []
-  index = 0
+
+# def repeat_number(array)
+#   array = [2,55,4,7,55]
+#   repeat_number = []
+#   index = 0
   
-  while index < array.length
-    if array[index] == 55
-      repeat_number << array[index]
-    end
-    index += 1
-  end
-  p repeat_number
+#   while index < array.length
+#     if array[index] == 55
+#       repeat_number << array[index]
+#     end
+#     index += 1
+#   end
+#   p repeat_number
 
-end
-repeat_number([2,55,7,55,8])
+# end
+# repeat_number([2,55,7,55,8])
 
-array.each do |number|
-  if number == 55
-    count += 1
-  end
-end 
-return count
-end
+# array.each do |number|
+#   if number == 55
+#     count += 1
+#   end
+# end 
+# return count
+# end
 
 
 
@@ -106,18 +122,18 @@ end
 # Write a method that accepts an array of strings and returns a new array that has the string "awesomesauce" inserted between every string. 
 # For example, if the initial array is ["a", "b", "c", "d", "e"], then the returned array should be ["a", "awesomesauce", "b", "awesomesauce", "c", "awesomesauce", "d", "awesomesauce", "e"].
 
-def awesomesauced(strings)
-  awesome_array = []
-  strings.each do |string|
-    awesome_array << string
-    awesome_array << "awesomesauce"
-  end
-  awesome_array.pop
-  p awesome_array
+# def awesomesauced(strings)
+#   awesome_array = []
+#   strings.each do |string|
+#     awesome_array << string
+#     awesome_array << "awesomesauce"
+#   end
+#   awesome_array.pop
+#   p awesome_array
   
 
-end
-awesomesauced(["a","b","c","d"])
+# end
+# awesomesauced(["a","b","c","d"])
 
 # Start with the hash: item_amounts = {chair: 5, table: 2}
 # Someone just bought two chairs. Change the hash such that the chair amount is 3.
@@ -130,9 +146,9 @@ awesomesauced(["a","b","c","d"])
 # You received 7 desks to sell. Change the hash to include desks.
 # The final result should be: {chair: 5, table: 2, desk: 7}
 
-item_amounts = {chair:5,table: 2}
-item_amounts[:desk] = 7
-p item_amounts
+# item_amounts = {chair:5,table: 2}
+# item_amounts[:desk] = 7
+# p item_amounts
 
 
 # Write a method that accepts a number and returns its factorial.
@@ -141,3 +157,70 @@ p item_amounts
 
 # Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array. 
 # For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+
+
+# Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
+# def select_even_items(strings)
+#   result = []
+#   index = 0
+#   strings.each do |string|
+#     if index % 2 == 0
+#       result << string
+#     end
+#     index = index + 1
+#   end
+#   result
+# end
+# p select_even_items(["a", "b", "c", "d", "e", "f"])
+
+
+# Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+
+# def max(numbers)
+#   current_max = numbers[0]
+#   numbers.each do |number|
+#     if number > current_max
+#       current_max = number
+#     end
+#   end
+#   current_max
+# end
+# p max([5, 4, 8, 1, 2])
+
+# Write a method that accepts a number and returns its factorial. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+def factorial(number)
+  result = 1
+  current_number = number
+  number.times do
+    result = result * current_number
+    current_number = current_number - 1
+  end
+  result
+end
+p factorial(5)
+
+
+# Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built into Ruby.
+def descending(numbers)
+  result = []
+  index = numbers.length - 1
+  numbers.length.times do
+    result << numbers[index]
+    index = index - 1
+  end
+  result
+end
+p descending([1, 3, 5, 7])
+
+
+# Write a method that accepts two arrays of numbers, and returns an array of every sum of every combination of single numbers from the first and second array. For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should return this array: [101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+def sum_combinations(numbers1, numbers2)
+  result = []
+  numbers1.each do |number1|
+    numbers2.each do |number2|
+      result << number1 + number2
+    end
+  end
+  result
+end
+p sum_combinations([1, 5, 10], [100, 500, 1000])
